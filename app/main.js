1bfa1a5996fa607b34c5ab2894085867aea8e374
= <gwelkind@gmail.com>
2016-05-18 17:22:00 -0500

    Cleaned up the ui a little

--- app/main.js
+++ app/main.js
@@ -1,7 +1,7 @@
 angular.module('main',['RecursionHelper'])
 .controller('bulletCtrl', function($scope){
 	$scope.rootBullet = testBulletRoot;
-	$scope.currentBullet = daddy2.children[0];
+	$scope.currentBullet = daddy2;
 	$scope.getAncestry = function(){
 		return $scope.currentBullet.getAncestry();
 	};
@@ -14,16 +14,18 @@ angular.module('main',['RecursionHelper'])
 				node: '=node',
 			},
 		template:
-			`<button class="btn btn-default" ng-click="node.toggleExpanded()" ng-show="node.children.length != 0">
-				<span ng-show="!node.expanded" ng-click="node.expand()" class="glyphicon glyphicon-plus" aria-hidden="true"></span>
-				<span ng-show="node.expanded" ng-click="node.contract()" class="glyphicon glyphicon-minus" aria-hidden="true"></span>
-			</button>
-			{{node.content}}{{bulletCtrl.testme}}
-			<ul class="list-group-sm" ng-show="node.expanded">
-				<li class="list-group-item" ng-repeat="child in node.children">
-					<bullet node="child"></bullet>
-				</li>
-			</ul>`,
+			`
+				<button class="btn btn-default" ng-click="node.toggleExpanded()" ng-show="node.children.length != 0">
+					<span ng-show="!node.expanded" class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+					<span ng-show="node.expanded" class="glyphicon glyphicon-minus" aria-hidden="true"></span>
+				</button>
+				{{node.content}}
+				<ul class="list-group-sm" ng-show="node.expanded">
+					<li class="list-group-item" ng-repeat="child in node.children">
+						<bullet node="child"></bullet>
+					</li>
+				</ul>
+			`,
 	  compile: function(element) {
 	      return RecursionHelper.compile(element, function(scope, iElement, iAttrs, controller, transcludeFn){
 
